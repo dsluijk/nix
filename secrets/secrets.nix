cfcaa9ea93a97ed39620689fe0c42576c5978368
Dany Sluijk <me@dany.dev>
2024-09-06 15:53:01 +0200

    feat(secrets): add the public key for paradise

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,10 @@
 let
   dsluijk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICPboZ99kJlrtxjdkUSmvVbgLicyEXrS4PGmXKBs7ptp me@dany.dev";
-  users = [ dsluijk ];
+  users = [dsluijk];
 
   odin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvl77vMSCzpGgMC6OCc2L8HluflBaHcrJKSHHm7/iMV";
-  systems = [ odin ];
-in
-{
+  paradise = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDaDZQr1pggZj1l9HjYXVhRljP7QiEVUNN2YJR/I2UiT";
+  systems = [odin paradise];
+in {
   "wireless.age".publicKeys = systems ++ users;
 }
