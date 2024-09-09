639dd23773a97367d02e65e7cb22ca71524c97a7
Dany Sluijk <me@dany.dev>
2024-09-09 23:55:00 +0200

    fix(outline): disable SSL mode for the database

--- modules/services/outline.nix
+++ modules/services/outline.nix
@@ -20,7 +20,7 @@ in {
       publicUrl = "https://write.dany.dev";
       forceHttps = false;
       storage.storageType = "local";
-      databaseUrl = "postgres://outline:@localhost:5432/outline";
+      databaseUrl = "postgres://outline:@localhost:5432/outline?sslmode=disable";
 
       oidcAuthentication = {
         authUrl = "https://login.dany.dev/application/o/authorize";
