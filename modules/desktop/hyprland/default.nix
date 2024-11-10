42781eb825f1cba74e421c1ef3fb98c0a9348dba
Dany Sluijk <me@dany.dev>
2024-11-10 22:52:41 +0100

    chore(desktop/hyprland): enable XWayland by default

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -10,7 +10,7 @@ with lib.my; let
 in {
   options.modules.desktop.hyprland = {
     enable = mkBoolOpt false;
-    xwayland = mkBoolOpt false;
+    xwayland = mkBoolOpt true;
     polkit = mkBoolOpt true;
   };
 
