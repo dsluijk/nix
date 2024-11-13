b05b1a3f28c266bde193bfc2144ec02b178e4c5f
Dany Sluijk <me@dany.dev>
2024-11-13 17:58:59 +0100

    feat(desktop/hyprland): add proper theming to Walker

--- modules/desktop/hyprland/walker.nix
+++ modules/desktop/hyprland/walker.nix
@@ -7,6 +7,8 @@
 with lib;
 with lib.my; let
   cfg = config.modules.desktop.hyprland;
+  colors = config.lib.stylix.colors;
+  rgb = color: "${colors."${color}-rgb-r"}, ${colors."${color}-rgb-g"}, ${colors."${color}-rgb-b"}";
 in {
   config = mkIf cfg.enable {
     home-manager.users.${config.modules.user.username} = {pkgs, ...}: {
@@ -21,6 +23,7 @@ in {
       programs.walker = {
         enable = true;
         runAsService = false;
+
         config = {
           terminal = "kitty";
           ignore_mouse = true;
@@ -76,6 +79,96 @@ in {
               ];
             }
           ];
+        };
+
+        theme = {
+          layout = {
+            ui = {
+              anchors = {
+                bottom = true;
+                left = true;
+                right = true;
+                top = true;
+              };
+
+              window = {
+                h_align = "fill";
+                v_align = "fill";
+
+                box = {
+                  orientation = "vertical";
+                  h_align = "center";
+                  v_align = "start";
+                  spacing = 10;
+                  width = 600;
+
+                  margins = {
+                    bottom = 200;
+                    top = 150;
+                  };
+
+                  search = {
+                    v_align = "start";
+                    spacing = 10;
+                    width = 600;
+                  };
+
+                  scroll = {
+                    list = {
+                      always_show = true;
+                      max_height = 600;
+                      max_width = 600;
+                      min_width = 600;
+                      width = 600;
+
+                      item = {
+                        spacing = 5;
+
+                        activation_label = {
+                          width = 20;
+                          x_align = 1;
+                        };
+
+                        icon = {
+                          theme = "Papirus";
+                        };
+
+                        text = {
+                          revert = true;
+                        };
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+
+          style = ''
+            #window {
+              background: none;
+            }
+
+            #box {
+              background: rgba(${rgb "base00"}, 1.0);
+              padding: 16px;
+              padding-top: 0px;
+              border-radius: 8px;
+              box-shadow:
+                0 19px 38px rgba(0, 0, 0, 0.3),
+                0 15px 12px rgba(0, 0, 0, 0.22);
+            }
+
+            #password,
+            #input,
+            #typeahead {
+              background: rgba(${rgb "base01"}, 0.8);
+              box-shadow: none;
+              color: rgba(${rgb "base05"}, 1.0);
+              padding-left: 12px;
+              padding-right: 12px;
+            }
+          '';
         };
       };
     };
