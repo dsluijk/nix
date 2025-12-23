84508234337b00b0d762e21fa703c365db6149a7
Dany Sluijk <me@dany.dev>
2025-12-23 14:14:30 +0100

    fix(service/headscale): sety global name servers

--- modules/services/headscale.nix
+++ modules/services/headscale.nix
@@ -24,6 +24,7 @@ in {
         dns = {
           base_domain = "tailnet.dany.dev";
           search_domains = ["dany.home"];
+          nameservers.global = ["1.1.1.1" "10.42.0.2"];
         };
 
         database = {
